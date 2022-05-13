6b9a94fcbd0850ed0a9b4cdbfc73bda7a479a4c4
Lucas-Braz7x <lucasdutrabraz@gmail.com>
2022-05-13 18:53:48 -0300

    Organização inicial

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Home } from './pages/Home';
+import { Container } from './sytles/styles';
+
+export const App = () => {
+  return (
+    <>
+      <Container>
+        <Home />
+      </Container>
+    </>
+  );
+};
