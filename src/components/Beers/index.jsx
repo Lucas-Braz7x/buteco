b92f5d4bdcb9f64e1462a11fd99bc1a31dc109df
Lucas-Braz7x <lucasdutrabraz@gmail.com>
2022-05-16 23:00:34 -0300

    Cria card personalizado

--- src/components/Beers/index.jsx
+++ src/components/Beers/index.jsx
@@ -1,32 +1,46 @@
 import React, { useEffect, useState } from 'react';
-import { BeerCard, BeerContainer, BeerDescription, BeerImage, BeerName } from './styles';
+import { BeersCard } from '../BeersCard';
+import { LoadMore } from '../LoadMore';
+import { BeerContainer } from './styles';
 
 export const Beers = () => {
   const [dataBeers, setDataBeers] = useState([]);
-  const teste = 0;
+  const [filteredBeers, setFilteredBeers] = useState([]);
+  const [updateBeers, setUpdateBeers] = useState(false);
   useEffect(() => {
     getContent();
-  }, [teste]);
+  }, []);
+
+  useEffect(() => {
+    loadMoreBeers();
+  }, [dataBeers, updateBeers]);
+
+  const loadMoreBeers = () => {
+    const count = filteredBeers.length;
+    setFilteredBeers((s) => s.concat(dataBeers.slice(count, count + 5)));
+  };
+
+  const handleUpdate = () => {
+    loadMoreBeers();
+    setUpdateBeers(!updateBeers);
+    console.log('data', dataBeers.length);
+    console.log('filter', filteredBeers.length);
+  };
 
   const getContent = () => {
-    fetch('https://api.punkapi.com/v2/beers?page=1&per_page=40')
+    fetch('https://api.punkapi.com/v2/beers?page=2&per_page=80')
       .then((response) => response.json())
       .then((data) => setDataBeers(data));
   };
   return (
-    <BeerContainer>
-      {dataBeers.map((beer, indice) => (
-        <BeerCard abv={beer.abv} ibu={beer.ibu} key={indice}>
-          <BeerName>{beer.name}</BeerName>
-          <BeerImage src={beer.image_url} alt={`Imagem da cerveja ${beer.name}`} />
-          <p>{beer.tagline}</p>
-          <BeerDescription>Descrição: {beer.description}</BeerDescription>
-          <div>
-            <span>abv: {beer.abv}</span>
-            <span>ibu: {beer.ibu}</span>
-          </div>
-        </BeerCard>
-      ))}
-    </BeerContainer>
+    <>
+      <BeerContainer>
+        {filteredBeers.map((beer, indice) => (
+          <BeersCard key={indice} beer={beer} />
+        ))}
+      </BeerContainer>
+
+      {dataBeers.length != filteredBeers.length && <LoadMore update={handleUpdate} />}
+    </>
   );
 };
--- src/sytles/global.js
+++ src/sytles/global.js
@@ -9,5 +9,6 @@ export default createGlobalStyle`
 
   html{
     font-size: 62.5%;
+    background: #ffffff;
   }
 `;
