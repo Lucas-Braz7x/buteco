27123b7374d9687acbf252c1d44241606d221049
Lucas-Braz7x <lucasdutrabraz@gmail.com>
2022-05-18 23:49:13 -0300

    Cria página de produtos

---
+++ src/pages/Product/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Beers } from '../../components/Beers';
+import { Search } from '../../components/Search';
+
+export const Product = () => {
+  return (
+    <>
+      <Search />
+      <Beers />
+    </>
+  );
+};
