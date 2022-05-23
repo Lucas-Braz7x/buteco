db845c65ed2a5604e23a3071a52ce09171ae8bf3
Lucas-Braz7x <lucasdutrabraz@gmail.com>
2022-05-22 22:05:03 -0300

    teste

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Header } from './components/Header';
 import { Home } from './pages/Home';
@@ -6,6 +6,9 @@ import { Product } from './pages/Product';
 import { Container, MainContainer } from './sytles/styles';
 
 export const App = () => {
+  useEffect(() => {
+    fetch('https://sp-labs.vercel.app/api/cases').then((response) => console.log(response));
+  });
   return (
     <BrowserRouter>
       <Container>
